3f4d2b08f3906f95bbf8bb5f1284ae18ed92c3df
luoyumo <yumo.luo@hotmail.com>
2018-05-26 11:57:10 +0300

    sovelluksen alustamisen yhteydessä suoritettava lähdekoodi

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+app.config["SECRET_KEY"]="12345678"
+
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///articles.db"
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
+
+from app import views
+from app import models
+db.create_all()
