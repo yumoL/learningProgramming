c3f10ab0d4f86bce612a651788cbc2caaa96a7af
luoyumo <yumo.luo@hotmail.com>
2018-06-02 08:08:31 +0300

    herokussa käytetään progresql

--- application/__init__.py
+++ application/__init__.py
@@ -4,18 +4,17 @@ import os
 app = Flask(__name__)
 
 
-# if os.environ.get("HEROKU"):
-#     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-# else:
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie.db"    
-app.config["SQLALCHEMY_ECHO"] = True
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
-app.config["SECRET_KEY"]="123"
-app.config["UP_DIR"]=os.path.join(os.path.abspath(os.path.dirname(__file__)),"static/uploads/")
-
-
+app.config["SECRET_KEY"] = "123"
+app.config["UP_DIR"] = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), "static/uploads/")
 
 
 from application.home import home as home_blueprint
@@ -25,7 +24,7 @@ from application import models
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint)
 
-try: 
+try:
     db.create_all()
 except:
     pass
